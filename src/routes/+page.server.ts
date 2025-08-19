6ce3cf4ac17fb7c4627c73442914431d09a5e609
paoloricciuti <ricciutipaolo@gmail.com>
2025-08-19 13:46:46 +0200

    chore: more setup

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,9 @@
+import { db } from '$lib/server/db';
+import { user } from '$lib/server/db/schema';
+
+export async function load() {
+	const users = await db.select().from(user).all();
+	return {
+		users
+	};
+}
